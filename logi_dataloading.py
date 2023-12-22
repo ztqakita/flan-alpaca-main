ca4c51fa50b64bebdd7af29465d3978e41dcaed5
charlielam0615 <lin.xiaohan@pku.edu.cn>
2023-12-22 15:08:15 +0800

    update to proofwriter dataset

--- logi_dataloading.py
+++ logi_dataloading.py
@@ -1,19 +1,40 @@
-import pytorch_lightning as pl
-from pytorch_lightning.callbacks import ModelCheckpoint
 from torch.utils.data.dataloader import DataLoader
 
-from logitorch.data_collators.ruletaker_collator import RuleTakerCollator
-from logitorch.datasets.qa.ruletaker_dataset import RuleTakerDataset
-from logitorch.pl_models.ruletaker import PLRuleTaker
+from logitorch.data_collators.proofwriter_collator import ProofWriterQACollator
+from logitorch.datasets.proof_qa.proofwriter_dataset import ProofWriterDataset
 
-train_dataset = RuleTakerDataset("depth-5", "train")
-val_dataset = RuleTakerDataset("depth-5", "val")
+# ==================== ProofWriter MetaInfo====================
 
-ruletaker_collate_fn = RuleTakerCollator()
+# PROOFWRITER_SUB_DATASETS = [
+#     "birds-electricity",
+#     "depth-0",
+#     "depth-1",
+#     "depth-2",
+#     "depth-3",
+#     "depth-3ext",
+#     "depth-3ext-NatLang",
+#     "depth-5",
+#     "NatLang",
+# ]
+# PROOFWRITER_TASKS = [
+#     "proof_generation_all",
+#     "proof_generation_iter",
+#     "implication_enumeration",
+#     "abduction",
+# ]
+
+# abducution task need open_world_assumption=False
+
+# =============================================================
+
+train_dataset = ProofWriterDataset(dataset_name="depth-5", split_set="train", task="proof_generation_all", open_world_assumption=True)
+val_dataset = ProofWriterDataset(dataset_name="depth-5", split_set="val", task="proof_generation_all", open_world_assumption=True)
+
+proofwriter_collate_fn = ProofWriterQACollator(pretrained_t5_tokenizer="google/t5-v1_1-small")
 
 train_dataloader = DataLoader(
-    train_dataset, batch_size=32, collate_fn=ruletaker_collate_fn
+    train_dataset, batch_size=32, collate_fn=proofwriter_collate_fn
 )
 val_dataloader = DataLoader(
-    val_dataset, batch_size=32, collate_fn=ruletaker_collate_fn
+    val_dataset, batch_size=32, collate_fn=proofwriter_collate_fn
 )
